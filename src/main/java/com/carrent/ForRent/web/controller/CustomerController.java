f363ae07e57a960d5249f8d36c4aada398b84922
Julie <nouguier.j3@gmail.com>
2023-05-15 17:01:50 +0200

    Method return Customer

--- src/main/java/com/carrent/ForRent/web/controller/CustomerController.java
+++ src/main/java/com/carrent/ForRent/web/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.carrent.ForRent.web.controller;
 
+import model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;//indicates that it is a REST controller
+
+import java.util.Calendar;
 
 @RestController //indicates that this class will be able to process
 public class CustomerController {
@@ -13,8 +16,11 @@ public class CustomerController {
     }
 
     @GetMapping("/Customer/{id}")
-    public String listCustomers(@PathVariable int id) {
-        return "Customer with id " + id;
+    public Customer listCustomers(@PathVariable int id) {
+        Calendar dateOfBirth = Calendar.getInstance();
+        dateOfBirth.set(1993, Calendar.JUNE, 13);
+        Customer customer = new Customer(id, "Nouguier", "Julie", dateOfBirth, "2A2004G52");
+        return customer;
 
     }
 }
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,70 @@
 package model;
 
+import java.util.Calendar;
+
 public class Customer {
+    private int id;
+    private String lastname;
+    private String firstname;
+    private Calendar dateOfBirth;
+    private String licenseNumber;
+
+    public Customer(int id, String lastname, String firstname, Calendar dateOfBirth, String licenseNumber) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.dateOfBirth = dateOfBirth;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Calendar getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Calendar dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id : " + id +
+                ", lastname : " + lastname + '\'' +
+                ", firstname : " + firstname + '\'' +
+                ", dateOfBirth : " + dateOfBirth +
+                ", licenseNumber : '" + licenseNumber + '\'' +
+                '}';
+    }
 }
