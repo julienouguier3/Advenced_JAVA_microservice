5be43c83893f2b9782a5d41708611741b9a2c3f9
Julie <nouguier.j3@gmail.com>
2023-05-15 14:38:41 +0200

    Init project

---
+++ src/main/java/com/carrent/ForRent/ForRentApplication.java
@@ -0,0 +1,13 @@
+package com.carrent.ForRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForRentApplication.class, args);
+	}
+
+}
