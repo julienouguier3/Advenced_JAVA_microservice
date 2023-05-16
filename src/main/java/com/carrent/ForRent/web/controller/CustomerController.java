66c80b084a9b4da20f4c5b1901685d8d49bcb858
Julie <nouguier.j3@gmail.com>
2023-05-16 14:36:00 +0200

    Add mapping CRUD

--- src/main/java/com/carrent/ForRent/web/controller/CustomerController.java
+++ src/main/java/com/carrent/ForRent/web/controller/CustomerController.java
@@ -1,26 +1,44 @@
 package com.carrent.ForRent.web.controller;
 
-import model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;//indicates that it is a REST controller
+import com.carrent.ForRent.dao.CustomerDao;
+import com.carrent.ForRent.model.Customer;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Calendar;
+import java.util.List;
 
 @RestController //indicates that this class will be able to process
 public class CustomerController {
 
-    @GetMapping("/Customer")
-    public String Customer() {
-        return "Customer list display";
+    private final CustomerDao customerDao;
+
+    public CustomerController(CustomerDao customerDao) {
+        this.customerDao = customerDao;
     }
 
-    @GetMapping("/Customer/{id}")
-    public Customer listCustomers(@PathVariable int id) {
-        Calendar dateOfBirth = Calendar.getInstance();
-        dateOfBirth.set(1993, Calendar.JUNE, 13);
-        Customer customer = new Customer(id, "Nouguier", "Julie", dateOfBirth, "2A2004G52");
-        return customer;
+    @GetMapping("/Customers")
+    public List<Customer> list() {
+        return customerDao.findAll();
 
+    }
+
+    @GetMapping(value = "/Customers/{id}")
+    public Customer show(@PathVariable int id) {
+        return customerDao.findById(id);
+
+    }
+
+    @PostMapping("/Customers")
+    public Customer add(@RequestBody Customer customer) {
+        return customerDao.save(customer);
+    }
+
+    @PutMapping("/Customers")
+    public Customer update(@RequestBody Customer customer) {
+        return customerDao.update(customer);
+    }
+
+    @DeleteMapping("/Customers/{id}")
+    public String delete(@PathVariable int id) {
+        return customerDao.delete(id);
     }
 }
